2616ba8046b053edfbff491a663c89abb75cd450
Dmitrii Morozov <33morozov@gmail.com>
2022-09-10 13:09:20 +0300

    Add 'task-manager33.herokuapp.com' in ALLOWED_HOST

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'task-manager33.herokuapp.com', ]
 
 
 # Application definition
