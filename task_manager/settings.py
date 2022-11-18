3b9fd8488437e5b16757e57c9da4fe09aa923889
Dmitrii Morozov <33morozov@gmail.com>
2022-11-18 13:11:54 +0300

    Something

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
