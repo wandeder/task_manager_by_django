f069d9af675f0dcc326ae0d720ac1cb8cf7719be
Dmitrii Morozov <33morozov@gmail.com>
2022-11-25 13:08:53 +0300

    Fixed bag with dependensew

--- task_manager/migrations/0001_initial.py
+++ task_manager/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
