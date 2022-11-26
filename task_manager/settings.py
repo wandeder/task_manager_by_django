46bf69df1fe89993a46dceebf5d688e2708b236b
Dmitrii Morozov <33morozov@gmail.com>
2022-11-26 20:34:15 +0300

    Fixed workflow

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# DATABASE_URL = os.getenv('DATABASE_URL')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+#if os.getenv('ON_PRODACT'):
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
