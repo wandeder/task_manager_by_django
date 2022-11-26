f3f977a5932ef6e1f7b29de38e4880b5294d7cc7
Dmitrii Morozov <33morozov@gmail.com>
2022-11-26 18:16:44 +0300

    Fixed workflow tests for GitHub Actions

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 # DATABASE_URL = os.getenv('DATABASE_URL')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+# }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
