d749d790a7b490bccb507ddb5b63ee991494b1a9
Dmitrii Morozov <33morozov@gmail.com>
2022-11-29 10:40:57 +0300

    Add translate fields for models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class user(AbstractUser):
@@ -8,7 +9,7 @@ class user(AbstractUser):
 
 
 class status(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -16,7 +17,7 @@ class status(models.Model):
 
 
 class label(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -24,13 +25,13 @@ class label(models.Model):
 
 
 class task(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=700)
-    status = models.ForeignKey(status, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    description = models.TextField(max_length=700, verbose_name=_('Description'))
+    status = models.ForeignKey(status, on_delete=models.PROTECT, verbose_name=_('Status'))
     creator = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='creator')
-    executor = models.ForeignKey(user, on_delete=models.PROTECT)
+    executor = models.ForeignKey(user, on_delete=models.PROTECT, verbose_name=_('Executor'))
     created_at = models.DateTimeField(auto_now_add=True)
-    labels = models.ManyToManyField(label, through="labels_of_task")
+    labels = models.ManyToManyField(label, through="labels_of_task", verbose_name=_('Labels'))
 
     def __str__(self):
         return self.name
