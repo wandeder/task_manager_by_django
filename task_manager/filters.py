fdfc495511f620b5aaccc834ee117a07df847e37
Dmitrii Morozov <33morozov@gmail.com>
2022-11-29 16:18:53 +0300

    Fixed fields of filter for task

--- task_manager/filters.py
+++ task_manager/filters.py
@@ -1,8 +1,10 @@
-from django_filters import FilterSet
-from task_manager.models import task
+from django_filters import FilterSet, ModelMultipleChoiceFilter
+from task_manager.models import task, label as label_model
+from django.utils.translation import gettext_lazy as _
 
 
 class TaskFilter(FilterSet):
+    labels = ModelMultipleChoiceFilter(queryset=label_model.objects.all(), label=_('Label'), conjoined=True)
 
     class Meta:
         model = task
